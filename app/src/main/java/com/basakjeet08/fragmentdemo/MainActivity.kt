addf7c3c3d221d3282f0a502bfc725be44e55774
Anirban Basak <jeetbasak2002@gmail.com>
2022-12-21 03:25:32 +0530

    Done With the Fragment Demos

--- app/src/main/java/com/basakjeet08/fragmentdemo/MainActivity.kt
+++ app/src/main/java/com/basakjeet08/fragmentdemo/MainActivity.kt
@@ -2,10 +2,22 @@ package com.basakjeet08.fragmentdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.basakjeet08.fragmentdemo.databinding.ActivityMainBinding
 
+private lateinit var binding : ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val bottomNavigationView = binding.bottomNavigationView
+        val navHostFragment =supportFragmentManager.findFragmentById(R.id.NavHostFragment) as NavHostFragment
+        val navController =navHostFragment.navController
+        bottomNavigationView.setupWithNavController(navController)
+
     }
 }
--- app/src/main/java/com/basakjeet08/fragmentdemo/fragments/SecondFragment.kt
+++ app/src/main/java/com/basakjeet08/fragmentdemo/fragments/SecondFragment.kt
@@ -5,14 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.basakjeet08.fragmentdemo.R
+import com.basakjeet08.fragmentdemo.databinding.FragmentSecondBinding
 
 class SecondFragment : Fragment() {
+    private lateinit var binding: FragmentSecondBinding
    override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        binding = FragmentSecondBinding.inflate(inflater , container , false)
+
+       binding.btn4thFragment.setOnClickListener{
+           findNavController().navigate(R.id.action_secondFragment_to_fourthFragment)
+       }
+       return binding.root
     }
 }
